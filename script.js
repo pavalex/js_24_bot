5c07394f84757b3be22e591a12a67068af735be2
pavalex <Alex.P79@mail.ru>
2022-04-03 12:36:33 +0800

    сделал бота

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 "use strict";
+
+let isStart = confirm('Поиграем?');
+
+const getStart = function (value) {
+    const number = 55;
+
+    const isNumber = function(num) {
+        if (num !== null && num.length > num.trim().length) {
+            return false;
+        }
+        
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    };
+
+    const getGuess = function (num) {    
+        let answer = prompt('Угадай число от 1 до 100');
+
+        if (answer === null) {
+            alert('Игра окончена');            
+        } else {
+            while (!isNumber(answer)) {
+                alert("Введи число!");
+                answer = prompt('Угадай число от 1 до 100');
+            }
+            
+            if (answer > num) {
+                alert('Загаданное число меньше');
+                getGuess(number);
+            } else if (answer < num) {
+                alert('Загаданное число больше');
+                getGuess(number);
+            } else {
+                alert('Поздравляю, Вы угадали!!!');
+            }
+        }        
+    };
+
+    if (value) {
+        getGuess(number);
+    } else {
+        alert('Игра окончена');
+    }
+};
+
+getStart(isStart);
